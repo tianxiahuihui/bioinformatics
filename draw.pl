375d461c2eff539571de6f1a19611e36cb4f1505
tianxiahuihui <2576138123@qq.com>
2016-09-17 17:09:24 +0800

    Update draw.pl

--- draw.pl
+++ draw.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl-w
 use strict;
-die  "\nUsage: $0 <InPutdir><Out>\n" unless (@ARGV == 3);
+die  "\nUsage: $0 <InPutdir><Out>\n" unless (@ARGV <= 3);
 my $input = "$ARGV[0]";
 my $outdir = "$ARGV[1]";
 my $sample = "$ARGV[2]";
@@ -18,7 +18,7 @@ print R "pdf(\"$outdir/$sample.pdf\",height=6,width=18)
 	a <- transform(a, period = factor(gene, levels = unique(gene)))
 	b<-as.data.frame(table(a\$num==1))
 	number<-b[1,2]
-	p <- ggplot(data = subset(a,a\$num==1), mapping = aes(x = period, y = dnmr)) + geom_bar(width = 0.5, stat = 'identity',fill = colorRampPalette(c(\'#F6D882\', \'#B81C31\'))(number)) + theme(axis.text.x=element_text(angle=45,colour='black',hjust=1,size = 10)) + labs(x = 'Gene Symbol', y = 'DNMR', title = 'Average') +  coord_cartesian(ylim=c(min(a\$dnmr)*0.95,max(a\$dnmr)*1.05))
+	p <- ggplot(data = subset(a,a\$num==1), mapping = aes(x = period, y = dnmr)) + geom_bar(width = 0.5, stat = 'identity',fill = colorRampPalette(c(\'#F6D882\', \'#B81C31\'))(number)) + theme_classic() + theme(axis.line.x=element_line(colour='black'),axis.line.y=element_line(colour='black'),axis.text.x=element_text(angle=45,colour='black',hjust=1,size = 10)) + labs(x = 'Gene Symbol', y = 'DNMR', title = 'DNMR-average') +  coord_cartesian(ylim=c(min(a\$dnmr)*0.95,max(a\$dnmr)*1.05))
 	print(p)	
 
 	dev.off()";
@@ -31,7 +31,7 @@ print R "pdf(\"$outdir/$sample.pdf\",height=6,width=18)
 	for (i in 1:max(a\$num)) {
 		b<-as.data.frame(table(a\$num==i))
 		number<-b[2,2]
-		p <- ggplot(data = subset(a,a\$num==i), mapping = aes(x = period, y = dnmr)) + geom_bar(width = 0.5, stat = 'identity',fill = colorRampPalette(c(\'#F6D882\', \'#B81C31\'))(number)) + theme(axis.text.x=element_text(angle=45,colour='black',hjust=1,size = 10)) + labs(x = 'Gene Symbol', y = 'DNMR', title = 'Average') +  coord_cartesian(ylim=c(min(a\$dnmr)*0.95,max(a\$dnmr)*1.05))
+		p <- ggplot(data = subset(a,a\$num==i), mapping = aes(x = period, y = dnmr)) + geom_bar(width = 0.5, stat = 'identity',fill = colorRampPalette(c(\'#F6D882\', \'#B81C31\'))(number)) + theme_classic() + theme(axis.line.x=element_line(size=.5,colour='black'),axis.line.y=element_line(colour='black'),axis.text.x=element_text(angle=45,colour='black',hjust=1,size = 10)) + labs(x = 'Gene Symbol', y = 'DNMR', title = 'DNMR-average') +  coord_cartesian(ylim=c(min(a\$dnmr)*0.95,max(a\$dnmr)*1.05))
 		print(p)
 	
 	}
